7ce048a00265fca371e1c9d892ce80f16379b65d
Peter Kitonga <pkitonga.pk@gmail.com>
2021-04-20 17:42:13 +0300

    Add missing typescript file

--- assets/scripts/first-app.ts
+++ assets/scripts/first-app.ts
@@ -1,11 +1,11 @@
 const button = document.getElementById("calculator-add-button");
 const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
-
+    
 const add = (num1: number, num2: number) => {
     return num1 + num2;
 }
-
+    
 button.addEventListener("click", () => {
     const calculator_result_span = document.getElementById("calculator-result-span");
     calculator_result_span.innerText = `RESULT: ${add(+input1.value, +input2.value)}`;
